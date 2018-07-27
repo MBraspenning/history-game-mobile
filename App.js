1f49a27e0a403038985860ec91df7fa3661b6d63
Matthijs Braspenning <matthijsbraspenning@gmail.com>
2018-07-27 23:08:44 +0200

    adds background image

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>History Game</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <ImageBackground
+                source={require('./assets/historyApp-bg2.jpg')}
+                style={{flex: 1}}
+            >
+                <View style={styles.container}>
+                    <Text>History Game</Text>    
+                </View>
+            </ImageBackground>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
