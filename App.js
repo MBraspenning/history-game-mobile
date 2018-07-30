27a120de103b13a65dd6e8dc4fb73f8b955867d6
Matthijs Braspenning <matthijsbraspenning@gmail.com>
2018-07-30 16:48:19 +0200

    sets up basic folder structure for app

--- App.js
+++ App.js
@@ -1,25 +1,3 @@
-import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import Index from './app/Index';
 
-export default class App extends React.Component {
-    render() {
-        return (
-            <ImageBackground
-                source={require('./assets/historyApp-bg2.jpg')}
-                style={{flex: 1}}
-            >
-                <View style={styles.container}>
-                    <Text>History Game</Text>    
-                </View>
-            </ImageBackground>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+export default Index;
