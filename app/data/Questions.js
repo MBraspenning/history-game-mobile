ccdd1cc88e1a7e2aec7726cae027d08ccaa69f05
Matthijs Braspenning <matthijsbraspenning@gmail.com>
2018-08-02 17:26:00 +0200

    initiates basic data provider

---
+++ app/data/Questions.js
@@ -0,0 +1,22 @@
+export const Questions = [
+    {
+        index: 1,
+        question: 'some thing history?',
+        option1: 'wrong',
+        option2: 'wrong',
+        option3: 'correct',
+        option4: 'wrong',
+        answer: 'option3',
+        trivia: 'some interesting fact'
+    },
+    {
+        index: 2,
+        question: 'some other thing history?',
+        option1: 'wrong',
+        option2: 'correct',
+        option3: 'wrong',
+        option4: 'wrong',
+        answer: 'option2',
+        trivia: 'some other interesting fact'
+    }
+]
