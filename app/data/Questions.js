2e547e592fd038be6249143e3447c19368cac5d4
Matthijs Braspenning <matthijsbraspenning@gmail.com>
2018-08-05 15:47:28 +0200

    adds question data

--- app/data/Questions.js
+++ app/data/Questions.js
@@ -1,26 +1,245 @@
 export const Questions = [
     {
         index: 1,
-        question: 'some thing history?',
+        question: 'Rome was one of the largest empires the world has ever seen. But can you name the first official emperor of Rome?',
         options: [
-            'wrong',
-            'wrong',
-            'correct',
-            'wrong'
+            'Julius Caesar',
+            'Mark Anthony',
+            'Caligula',
+            'Augustus'
         ],
-        answer: 3,
-        trivia: 'some interesting fact'
+        answer: 4,
+        trivia: 'The Roman empire is the longest-lasting empire in history. It spanned several eras, but essentially lasted from 27 BC to 1453 AD. A grand total of 1,480 years.'
     },
     {
         index: 2,
-        question: 'some other thing history?',
+        question: 'The Battle of Hastings was won by William the Conqueror and started the Norman conquest of England. But in what year did the battle take place?',
         options: [
-            'wrong',
-            'correct',
-            'wrong',
-            'wrong'
+            '1302',
+            '1066',
+            '814',
+            '1099'
         ],
         answer: 2,
-        trivia: 'some other interesting fact'
+        trivia: 'The Battle of Hastings took place 11 km north of Hastings at the present-day town of Battle which got it\'s name from this historical event.'
+    },
+    {
+        index: 3,
+        question: 'In 1865, following the American Civil War, president Abraham Lincoln was shot and killed by a Southern sympathizer. Do you know the name of the shooter?',
+        options: [
+            'Robert E. Lee',
+            'Andrew Johnson',
+            'John Wilkes Booth',
+            'Joseph E. Johnston'
+        ],
+        answer: 3,
+        trivia: 'Many surveys and polls conducted in the last 100 years voted Abraham Lincoln as the most popular president in American history.'
+    },
+    {
+        index: 4,
+        question: 'The Aztec Empire was conquered by the Spanish Conquistadors in the 16th century. Can you name the man who led the expedition which caused the fall of the Aztec Empire?',
+        options: [
+            'Christopher Columbus',
+            'Diego Velazquez',
+            'Hernan Cortes',
+            'Don Diego de la Vega'
+        ],
+        answer: 3,
+        trivia: 'The Aztec capital Tenochtitlan was destroyed during the conquest of the Aztec Empire. The Spanish conquerors renamed it Mexico City which now is the capital city of Mexico.'
+    },
+    {
+        index: 5,
+        question: 'During the final stages of WWII, the U.S. dropped two atomic bombs on the Japanese cities Hiroshima and Nagasaki. What was the name of the first plane in history to drop an atomic bomb?',
+        options: [
+            'Manhattan',
+            'Enola Gay',
+            'Spitfire',
+            'SBD Dauntless'
+        ],
+        answer: 2,
+        trivia: 'Japan\'s refusal to accept the Allies\' demands for unconditional surrender led to the bombing of Hiroshima and Nagasaki. Six days after the second bomb dropped, Japan finally surrendered and World War II came to an end.'
+    },
+    {
+        index: 6,
+        question: 'Today, Trafalgar Square is a lasting reminder of the British naval victory of the Battle of Trafalgar in the Napoleonic Wars. Do you know who commanded the British naval forces during the battle?',
+        options: [
+            'Francis Drake',
+            'Robert Maynard',
+            'Horatio Nelson',
+            'Edward Smith'
+        ],
+        answer: 3,
+        trivia: 'The HMS Victory, the flagship of the British commander at the Battle of Trafalgar, still serves as the flagship of the First Sea Lord and is the oldest naval ship still in commission.'
+    },
+    {
+        index: 7,
+        question: 'The Vikings raided England on multiple occasions and played a big role in the history of Britain. But do you know the name of the place where the Vikings first landed in Britain?',
+        options: [
+            'Lindisfarne',
+            'York',
+            'Isle of Sheppey',
+            'Dommoc'
+        ],
+        answer: 1,
+        trivia: 'The word "viking" is derived from the Old English word "wicing" which translates as pirate, a fitting term for a seafaring people known best for their violent raids.'
+    },
+    {
+        index: 8,
+        question: 'The outbreak of World War I can be traced back to the assassination of Archduke Franz Ferdinand of Austria. But what was the name of the assassin?',
+        options: [
+            'Gavrilo Princip',
+            'Oskar Potiorek',
+            'Dragutin Dimitrijević',
+            'Vlade Divac'
+        ],
+        answer: 1,
+        trivia: 'The Scottish rock band Franz Ferdinand choose their name after seeing a racehorse named Archduke Ferdinand win the Northumberland Plate in 2001.'
+    },
+    {
+        index: 9,
+        question: 'In the 17th and 18th century pirates terrorised the worlds oceans and seas. They are associated with the iconic black flag with skull and crossbones. But what is traditional English name for this flag?',
+        options : [
+            'Black John',
+            'Smiling Sam',
+            'Jolly Roger',
+            'Union Jack'
+        ],
+        answer: 3,
+        trivia: 'Pirates would normally fly false colors or no colors at all until they had their prey within firing range. At that point they would raise their own flag, often simultaneously with a warning shot.'
+    },
+    {
+        index: 10,
+        question: 'On the 15th of April 1912 the RMS Titanic sank after it collided with an iceberg during its maiden voyage from Southampton to New York. Do you know the name of the ship\'s captain?',
+        options: [
+            'William Thomas Turner',
+            'Arthur Rostron',
+            'Robert Fitzroy',
+            'Edward Smith'
+        ],
+        answer: 4,
+        trivia: 'The RMS Titanic was one of three Olympic-class ocean liners built for the White Star Line in the early 20th century. The other two being the RMS Olympic and the RMS Britannic.'
+    },
+    {
+        index: 11,
+        question: 'In 1969, the Lunar Module Apollo 11 landed on the moon. Neil Armstrong was the first man to walk on the moon, but who was the second man to set foot on the moon\'s surface?',
+        options: [
+            'Jim Lovell',
+            'Buzz Aldrin',
+            'Yuri Gagarin',
+            'James Irwin'
+        ],
+        answer: 2,
+        trivia: 'The second man on the moon was tasked with taking a series of panoramic photos while on the moon, so many of the famous photos we know today were taken by this man.'
+    },
+    {
+        index: 12,
+        question: 'The expression "cogito ergo sum", meaning "I think, therefore I am" is one we all know, but who wrote this philosophical proposition?',
+        options: [
+            'Voltaire',
+            'Plato',
+            'René Déscartes',
+            'Aristotle'
+        ],
+        answer: 3,
+        trivia: 'The underlying meaning of the phrase is that the very act of doubting one\'s own existence serves as proof of the reality of one\'s own mind.'
+    },
+    {
+        index: 13,
+        question: 'At the Battle of Waterloo Napoleon Bonaparte was defeated by two armies of the Seventh Coalition. One of the victorious armies was led by a British commander, what was his name?',
+        options: [
+            'Arthur Wellesley',
+            'William II',
+            'Bernard Montgomery',
+            'Arthur Percival'
+        ],
+        answer: 1,
+        trivia: 'Tourism at the battleground started the day after the battle. The iconic monument we can see today was ordered by King William I of the Netherlands in 1820.'
+    },
+    {
+        index: 14,
+        question: 'In the year 313 AD the Western Roman Emperor Constantine I issued an edict which gave Christians a legal status throughout the empire. What was the place where it was issued?',
+        options: [
+            'Rome',
+            'Constantinople',
+            'Milan',
+            'Florence'
+        ],
+        answer: 3,
+        trivia: 'Christianity became the official religion of the Roman Empire in the year 380 AD under emperor Theodosius I.'
+    },
+    {
+        index: 15,
+        question: 'By the turn of the 19th century Russia was one of the most powerfull empires in the world. This came to an end in 1917 when the last tsar was forced to abdicate. What was his name?',
+        options: [
+            'Vladimir Lenin',
+            'Nicholas Romanov',
+            'Ivan Vasilyevich',
+            'Peter the Great'
+        ],
+        answer: 2,
+        trivia: 'Russia\'s war with Germany in WWI caused 3,300,000 Russian deaths. This in combination with incompetent management of the war effort and lack of food led to the fall of the last tsar of Russia.'
+    },
+    {
+        index: 16,
+        question: 'The national holiday of France is the 14th of July and remembers a particular event which happened in 1789 that started the French Revolution. What was that event?',
+        options: [
+            'Execution of Louis XVI',
+            'Napoleon became emperor',
+            'Storming of the Bastille',
+            'Financial Crisis'
+        ],
+        answer: 3,
+        trivia: 'The French royal family tried to escape the country but were caught at the border. Even though people had no pictures of the royal family, they were recognized because of coins which showed the king\'s face.'
+    },
+    {
+        index: 17,
+        question: 'The year 1961 saw the construction of the Berlin Wall, dividing the German capital in two parts. But do you know when the wall came down, thus reuniting Berlin and Germany.',
+        options: [
+            '1989',
+            '1992',
+            '1985',
+            '1969'
+        ],
+        answer: 1,
+        trivia: 'The Berlin Wall was constructed as a way of preventing East Germans from entering West Germany. It was not so much a boundary for West Germans wanting to enter the East, who were able to do so by obtaining a permit.'
+    },
+    {
+        index: 18,
+        question: 'In 1095 Pope Urban II held a sermon in Clermont. This led to a major event in human history. Do you know which one?',
+        options: [
+            'The Hundred Years\' War',
+            'The First Crusade',
+            'The Great Schism',
+            'The Rise of Protestantism'
+        ],
+        answer: 2,
+        trivia: 'Pope Urban II laid the foundations for the papal monarchy, and his pontificate marked a turning point in the institutional organization of the papacy and in papal-imperial relations.'
+    },
+    {
+        index: 19,
+        question: 'In the 2nd century the Romans wanted to protect their borders in Britain by constructing a giant wall. Can you name the Emperor whose name is still attached to this wall?',
+        options: [
+            'Hadrian',
+            'Trajan',
+            'Marcus Aurelius',
+            'Tiberius'
+        ],
+        answer: 1,
+        trivia: 'George R. R. Martin has acknowledged that this wall was the inspiration for The Wall in his best-selling series A Song of Ice and Fire, dramatised in the TV series Game of Thrones.'
+    },
+    {
+        index: 20,
+        question: '1986 witnessed a devastating disaster in space travel. The space shuttle disintegrated 73 seconds into it\'s flight, killing all 7 crew members. What was the name of that space shuttle?',
+        options: [
+            'Columbia',
+            'Discovery',
+            'Challenger',
+            'Atlantis'
+        ],
+        answer: 3,
+        trivia:'Christa McAuliffe was selected from more than 11,000 applicants to participate in the NASA Teacher in Space Project and was scheduled to become the first teacher in space. Unfortunately she died together with the other crew members.'
     }
 ]
+
+
+
