5e8fa363bea6aa9e4e8a68db61a76de92a34ab2b
Matthijs Braspenning <matthijsbraspenning@gmail.com>
2018-08-06 19:36:12 +0200

    adds data for when game is finished

---
+++ app/data/Finished.js
@@ -0,0 +1,13 @@
+export const Finished = {
+    header: [
+        'You came, you saw, you conquered!',
+        'Good job!',
+        'Keep at it!'
+    ],
+    content: [
+        'You conquered the game just like Julius Caesar conquered Gaul, ave!',
+        'You are well on your way to become the next Cicero, remember his famous saying: to be ignorant of the past is to always remain a child.',
+        'There is some room for improvement, but you know what they say: "Rome wasn\'t built in a day".'
+    ],
+    trivia: '"Those who do not remember the past are condemned to repeat it."  - George Santayana'
+}
